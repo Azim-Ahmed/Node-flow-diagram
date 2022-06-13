be9e4774477ad1b82f0bb0cadd054f91e4d38eb2
Azim Uddin Ahamed <azim@sqbear.com.au>
2022-06-13 22:01:49 +0600

    deleted the some nodes types

--- src/assets/FlowData/index.js
+++ src/assets/FlowData/index.js
@@ -6,13 +6,13 @@ export const flowData = [
         color: "#42a5f5",
         title: "Drag this INPUT_1"
     },
-    {
-        style: "bg-blue-200 w-40 my-2 py-2 mx-2 rounded-md cursor-grab shadow-lg",
-        type: "customInput",
-        text: "INPUT_2",
-        color: "#90caf9",
-        title: "Drag this INPUT_2"
-    },
+    // {
+    //     style: "bg-blue-200 w-40 my-2 py-2 mx-2 rounded-md cursor-grab shadow-lg",
+    //     type: "customInput",
+    //     text: "INPUT_2",
+    //     color: "#90caf9",
+    //     title: "Drag this INPUT_2"
+    // },
     {
         style: "bg-blue-200 w-40 my-2 py-2 mx-2 rounded-md cursor-grab shadow-lg",
         type: "decision",
@@ -27,14 +27,14 @@ export const flowData = [
         color: "#80cbc4",
         title: "Drag this NODE_EXEC_1"
     },
-    {
-        style: "bg-teal-400 w-40 my-2 py-2 mx-2 rounded-md cursor-grab shadow-lg",
-        type: "customFunction",
-        text: "NODE_EXEC_2",
-        color: "#26a69a",
-        title: "Drag this NODE_EXEC_2"
+    // {
+    //     style: "bg-teal-400 w-40 my-2 py-2 mx-2 rounded-md cursor-grab shadow-lg",
+    //     type: "customFunction",
+    //     text: "NODE_EXEC_2",
+    //     color: "#26a69a",
+    //     title: "Drag this NODE_EXEC_2"
 
-    },
+    // },
     {
         style: "bg-blue-200 w-40 my-2 py-2 mx-2 rounded-md cursor-grab shadow-lg",
         type: "circle",
@@ -48,11 +48,11 @@ export const flowData = [
         color: "#7986cb",
         title: "Drag this OUTPUT_1"
     },
-    {
-        style: "bg-indigo-400 w-40 my-2 py-2 mx-2 rounded-md shadow-lg",
-        type: "customOutput",
-        text: "OUTPUT_2",
-        color: "#5c6bc0",
-        title: "Drag this OUTPUT_2"
-    },
+    // {
+    //     style: "bg-indigo-400 w-40 my-2 py-2 mx-2 rounded-md shadow-lg",
+    //     type: "customOutput",
+    //     text: "OUTPUT_2",
+    //     color: "#5c6bc0",
+    //     title: "Drag this OUTPUT_2"
+    // },
 ];
